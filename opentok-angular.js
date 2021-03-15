e96ee340d287566c1a9430c640a04b41426baec7
Michael Whittemore <michael.whittemore@vonage.com>
2021-03-15 16:52:10 -0700

    publish audio false

--- opentok-angular.js
+++ opentok-angular.js
@@ -130,7 +130,7 @@ ng.module('opentok', [])
         link: function(scope, element, attrs) {
           var props = scope.props() || {};
           props.videoSource = false; // no video
-          props.audioSource = false; // no audio
+          props.publishAudio = false; // no audio, but still need a source
           props.width = props.width ? props.width : ng.element(element).width();
           props.height = props.height ? props.height : ng.element(element).height();
           var oldChildren = ng.element(element).children();
